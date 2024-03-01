39350810fe6a769f0210a710f1d1d5b2e89acc43
Yung, Chun Kau <yung.chun.kau@gmail.com>
2024-03-02 02:58:22 +0800

    Add options '-T','-i','-w','-n','-c','-l','-v'

---
+++ GetOptions.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace grep;
+
+record FlagedArg(bool Flag, string Arg);
+
+static class Options
+{
+    public static IEnumerable<FlagedArg> ToFlagedArgs(
+        this IEnumerable<string> args,
+        ImmutableDictionary<string, string> mapShortcuts,
+        ImmutableDictionary<string, string[]> mapExpandingShortcuts)
+    {
+        var itrThe = args.GetEnumerator();
+        IEnumerable<string> SeparateCombiningShortcut()
+        {
+            while (itrThe.MoveNext())
+            {
+                var current = itrThe.Current;
+                if (current.StartsWith("--"))
+                {
+                    yield return current;
+                }
+                else if (current.StartsWith("-") && current.Length > 2)
+                {
+                    foreach (var charThe in current.Skip(1))
+                    {
+                        yield return "-" + charThe;
+                    }
+                }
+                else
+                {
+                    yield return current;
+                }
+            }
+        }
+
+        foreach (var arg in SeparateCombiningShortcut())
+        {
+            if (arg.Length == 2 && arg[0] == '-')
+            {
+                if (mapShortcuts.TryGetValue(arg, out var found))
+                {
+                    yield return new(false, found);
+                }
+                else if (mapExpandingShortcuts.TryGetValue(arg, out var expands))
+                {
+                    foreach (var expand in expands)
+                    {
+                        yield return new(false, expand);
+                    }
+                }
+                else
+                {
+                    yield return new(false, arg);
+                }
+            }
+            else
+            {
+                yield return new(false, arg);
+            }
+        }
+    }
+
+    static IEnumerable<FlagedArg> SelectWithFlag(IEnumerable<FlagedArg> args,
+        string name)
+    {
+        var itrThe = args.GetEnumerator();
+        while (itrThe.MoveNext())
+        {
+            var current = itrThe.Current;
+            if (current.Arg == name)
+            {
+                if (itrThe.MoveNext())
+                {
+                    yield return new FlagedArg(true, itrThe.Current.Arg);
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        $"Missing value to {name}");
+                }
+            }
+            else
+            {
+                yield return current;
+            }
+        }
+    }
+
+    public static (Func<T, R>, IEnumerable<FlagedArg>) Parse<T, R>(
+        IEnumerable<FlagedArg> args, string name,
+        Func<T, R> init, Func<string, Func<T, R>> parse)
+    {
+        var qryFlaged = SelectWithFlag(args, name)
+            .GroupBy((it) => it.Flag)
+            .ToDictionary((grp) => grp.Key,
+            elementSelector: (grp) => grp.AsEnumerable());
+
+        IEnumerable<FlagedArg>? qryNotFound;
+        if (false == qryFlaged.TryGetValue(false, out qryNotFound))
+        {
+            qryNotFound = Array.Empty<FlagedArg>();
+        }
+
+        if (qryFlaged.TryGetValue(true, out var qryFound))
+        {
+            var foundThe = qryFound.Distinct().ToArray();
+            if (foundThe.Length > 1)
+            {
+                throw new ArgumentException(
+                    $"Too many value ('{foundThe[0].Arg}','{foundThe[1].Arg}') to {name}");
+            }
+            return (parse(foundThe[0].Arg), qryNotFound);
+        }
+        return (init, qryNotFound);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 
 namespace grep;
+
+using static grep.Options;
 
 class Program
 {
@@ -10,85 +13,273 @@ class Program
         {
             RunMain(args);
         }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
         catch (Exception ee)
         {
             Console.WriteLine(ee);
         }
     }
 
-    static bool PrintSyntax()
+    static bool PrintSyntax(bool isDetailed = false)
     {
+        Console.WriteLine(isDetailed ? $"{nameof(grep)} -?" : $"{nameof(grep)} --help");
         Console.WriteLine($"""
             {nameof(grep)} [OPTIONS] REGEX FILE [FILE ..]
             {nameof(grep)} [OPTIONS] REGEX --files-from FILES-FROM
-            {nameof(grep)} [OPTIONS] REGEX -T FILES-FROM            
             """);
+        if (isDetailed)
+        {
+            Console.WriteLine($"""
+
+            OPTIONS:            DEFAULT  ALTERATIVE
+              --verbose         off      on
+              --case-sensitive  on       off
+              --word            off      on
+              --line-number     off      on
+              --count           off      on
+              --file-match      off      on
+              --invert-match    off      on
+            
+            Short-Cut
+              -T       --files-from
+              -i       --case-sensitive off
+              -w       --word on
+              -n       --line-number on
+              -c       --count on
+              -l       --file-match on
+              -v       --invert-match on
+
+            Read console input if FILES-FROM is -
+            """);
+        }
         return false;
     }
 
-    const Char SpecThe = (Char)27;
+    const string OptFilesFrom = "--files-from";
+    const string OptCaseSensitive = "--case-sensitive";
+    const string OptWord = "--word";
+    const string OptLineNumber = "--line-number";
+    const string OptCountOnly = "--count";
+    const string OptFileMatch = "--file-match";
+    const string OptInvertMatch = "--invert-match";
+
     static bool RunMain(string[] args)
     {
+        if (args.Any((it) => it == "--help")) return PrintSyntax(true);
         if (args.Length < 2) return PrintSyntax();
 
         var qry = from arg in args
-                  join help in new string[] { "-?", "-h", "--help" }
+                  join help in new string[] { "-?", "-h"}
                   on arg equals help into helpQuery
                   from found in helpQuery select found;
         if (qry.Any()) return PrintSyntax();
 
-        var SpecText = SpecThe.ToString();
-        var oldColor = Console.ForegroundColor;
-        var redColor = ConsoleColor.Red;
+        var flagedArgs = args.ToFlagedArgs(new Dictionary<string, string>()
+        {
+            ["-T"] = OptFilesFrom,
+        }.ToImmutableDictionary(), new Dictionary<string, string[]>()
+        {
+            ["-i"] = [OptCaseSensitive, "off"],
+            ["-w"] = [OptWord, "on"],
+            ["-n"] = [OptLineNumber, "on"],
+            ["-c"] = [OptCountOnly, "on"],
+            ["-l"] = [OptFileMatch, "on"],
+            ["-v"] = [OptInvertMatch, "on"],
+        }.ToImmutableDictionary());
 
-        var regEx = new Regex(args[0]);
-        foreach (var filename in args
+        (var filesFrom, flagedArgs) = Parse<bool, IEnumerable<string>>(flagedArgs,
+            name: OptFilesFrom, init: (_) => Array.Empty<string>(),
+            parse: (path) =>
+            {
+                string? lineThe;
+                if ("-" == path)
+                {
+                    IEnumerable<string> PathsFromConsole()
+                    {
+                        while (null != (lineThe = Console.ReadLine()))
+                        {
+                            yield return lineThe;
+                        }
+                    }
+                    return (_) => PathsFromConsole();
+                }
+                if (true != File.Exists(path))
+                    throw new ArgumentException($"'{path}' to {OptFilesFrom} is NOT found!");
+                var inpFs = File.OpenText(path);
+                IEnumerable<string> FilesFromFile()
+                {
+                    while (null != (lineThe = inpFs.ReadLine()))
+                    {
+                        yield return lineThe;
+                    }
+                    inpFs.Close();
+                }
+                return (_) => FilesFromFile();
+            });
+
+        (var toWord, flagedArgs) = Parse<string, string>(flagedArgs,
+            name: OptWord, init: (it) => it,
+            parse: (flag) =>
+            {
+                if (flag == "on") return (it) => @"\s" + it + @"\s";
+                return (it) => it;
+            });
+
+        (var makeRegex, flagedArgs) = Parse<string, Regex>(flagedArgs,
+            name: OptCaseSensitive, init: (it) => new Regex(it),
+            parse: (flag) =>
+            {
+                if (flag == "off") return (it) =>
+                new Regex(it, RegexOptions.IgnoreCase);
+                return (it) => new Regex(it);
+            });
+
+        (var _, flagedArgs) = Parse<bool, bool>(flagedArgs,
+            name: OptLineNumber, init: (_) => true,
+            parse: (flag) =>
+            {
+                if (flag == "on")
+                {
+                    PrintLineNumber = (it) => Console.Write($"{it}:");
+                    return (_) => true;
+                };
+                return (_) => true;
+            });
+
+        (var postMatch, flagedArgs) = Parse<PostMatchParam, bool>(flagedArgs,
+            name: OptCountOnly, init: (it) => DefaultPostMatch(it),
+            parse: (flag) =>
+            {
+                if (flag == "on")
+                {
+                    PrintFoundCount = (path, count) =>
+                    {
+                        if (1 > count) return;
+                        Console.WriteLine($"{path}:{count}");
+                    };
+                    return (_) => true;
+                };
+                return (it) => DefaultPostMatch(it);
+            });
+
+        (var checkMatches, flagedArgs) = Parse<MatchCollection, bool>(flagedArgs,
+            name: OptInvertMatch, init: (matches) => matches.Any(),
+            parse: (flag) =>
+            {
+                if (flag == "on")
+                {
+                    DefaultPostMatch = (it) =>
+                    {
+                        Console.WriteLine(it.Path + ":");
+                        PrintLineNumber(it.Index);
+                        Console.WriteLine(it.Line);
+                        return true;
+                    };
+                    return (matches) => true != matches.Any();
+                }
+                return (matches) => matches.Any();
+            });
+
+        (var _, flagedArgs) = Parse<bool, bool>(flagedArgs,
+            name: OptFileMatch, init: (_) => true,
+            parse: (flag) =>
+            {
+                if (flag == "on")
+                {
+                    var flagFound = false;
+                    postMatch = (_) =>
+                    {
+                        flagFound = true;
+                        return false;
+                    };
+                    PrintFoundCount = (path, count) =>
+                    {
+                        if (flagFound) Console.WriteLine(path);
+                        flagFound = false;
+                    };
+                };
+                return (_) => true;
+            });
+
+        var argsRest = flagedArgs.Select((it) => it.Arg).ToArray();
+        if (argsRest.Length < 1)
+        {
+            return PrintSyntax();
+        }
+
+        var regEx = makeRegex(toWord(argsRest.First()));
+
+        foreach (var filename in filesFrom(true)
+            .Union(
+            argsRest
             .Skip(1)
+            .Select((it) => it.Trim())
+            .Distinct())
             .Where((it) => File.Exists(it)))
         {
-            using (var inpFs = File.OpenText(filename))
+            using var inpFs = File.OpenText(filename);
+            int cntLine = 0;
+            int cntFound = 0;
+            string? lineRead;
+            while (null != (lineRead = inpFs.ReadLine()))
             {
-                int cntLne = 0;
-                string? lineRead;
-                while (null != (lineRead = inpFs.ReadLine()))
+                cntLine += 1;
+                var matches = regEx.Matches(lineRead);
+                if (true != checkMatches(matches)) continue;
+                cntFound += 1;
+                if (true != postMatch(new(filename, cntLine, lineRead, matches)))
                 {
-                    cntLne += 1;
-                    var matches = regEx.Matches(lineRead);
-                    if (true != matches.Any()) continue;
-                    var foundQueue = new List<(int,int)>();
-                    var lastIndex = 0;
-                    foreach (Match match in matches)
-                    {
-                        System.Diagnostics.Debug.WriteLine(
-                            $"line#{cntLne}: {match.Index},{match.Length}");
-                        foundQueue.Add((match.Index, match.Length));
-                        lastIndex = match.Index + match.Length;
-                    }
-                    Console.Write($"{filename}:");
-                    var ndxLast = 0;
-                    foreach ((var ndx, var len) in foundQueue)
-                    {
-                        switch (ndxLast, ndx)
-                        {
-                            case (0, 0): break;
-                            default:
-                                Console.ForegroundColor = oldColor;
-                                Console.Write(lineRead.Substring(ndxLast, ndx - ndxLast));
-                                break;
-                        }
-                        Console.ForegroundColor = redColor;
-                        Console.Write(lineRead.Substring(ndx, len));
-                        ndxLast = ndx + len;
-                    }
-                    Console.ResetColor();
-                    if (ndxLast < lineRead.Length)
-                    {
-                        Console.Write(lineRead.Substring(ndxLast));
-                    }
-                    Console.WriteLine();
+                    break;
                 }
             }
+            PrintFoundCount(filename, cntFound);
         }
         return true;
     }
+
+    static ConsoleColor OldColor { get; set; } = Console.ForegroundColor;
+    static ConsoleColor RedColor { get; set; } = ConsoleColor.Red;
+
+    static Action<int> PrintLineNumber { get; set; } = (_) => { };
+    static Action<string, int> PrintFoundCount { get; set; } = (_, _)  => { };
+    record PostMatchParam(string Path, int Index, string Line, MatchCollection Matches);
+
+    static Func<PostMatchParam, bool> DefaultPostMatch { get; set; } = (param) =>
+    {
+        (var filename, var lineNbr, var lineRead, var matches) = param;
+        var foundQueue = new List<(int, int)>();
+        var lastIndex = 0;
+        foreach (Match match in matches)
+        {
+            foundQueue.Add((match.Index, match.Length));
+            lastIndex = match.Index + match.Length;
+        }
+        Console.Write($"{filename}:");
+        PrintLineNumber(lineNbr);
+        var ndxLast = 0;
+        foreach ((var ndx, var len) in foundQueue)
+        {
+            switch (ndxLast, ndx)
+            {
+                case (0, 0): break;
+                default:
+                    Console.ForegroundColor = OldColor;
+                    Console.Write(lineRead[ndxLast..ndx]);
+                    break;
+            }
+            Console.ForegroundColor = RedColor;
+            Console.Write(lineRead[ndx..(ndx + len)]);
+            ndxLast = ndx + len;
+        }
+        Console.ResetColor();
+        if (ndxLast < lineRead.Length)
+        {
+            Console.Write(lineRead[ndxLast..]);
+        }
+        Console.WriteLine();
+        return true;
+    };
 }
